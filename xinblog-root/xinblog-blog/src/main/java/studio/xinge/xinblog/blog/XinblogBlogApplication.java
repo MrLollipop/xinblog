93bb63c3cd08c4071ca2eda89139ee705edb58f5
haoxin_2014 <haoxin_2014@163.com>
2021-07-16 12:51:13 +0800

    博客微服务初始化

---
+++ xinblog-root/xinblog-blog/src/main/java/studio/xinge/xinblog/blog/XinblogBlogApplication.java
@@ -0,0 +1,13 @@
+package studio.xinge.xinblog.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinblogBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinblogBlogApplication.class, args);
+    }
+
+}
---
+++ xinblog-root/xinblog-blog/src/test/java/studio/xinge/xinblog/blog/XinblogBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.xinge.xinblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinblogBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
