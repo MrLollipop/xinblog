b5904820cb2a3ae6e383837b79b694a8ff2f00ca
haoxin_2014 <haoxin_2014@163.com>
2021-09-01 11:43:14 +0800

    博客表表结构更新，添加回复表

--- db/xinblog_bms.sql
+++ db/xinblog_bms.sql
@@ -1,10 +1,12 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2021/7/19 15:22:23                           */
+/* Created on:     2021/9/1 11:01:40                            */
 /*==============================================================*/
 
 
 drop table if exists bms_blog;
+
+drop table if exists bms_blog_reply;
 
 /*==============================================================*/
 /* Table: bms_blog                                              */
@@ -14,11 +16,34 @@ create table bms_blog
    id                   bigint not null auto_increment,
    title                varchar(30),
    content              varchar(1000),
-   status               tinyint,
+   status               tinyint comment '0删除，1正常，2草稿',
    create_time          datetime,
    update_time          datetime,
+   like_num             int,
+   forward_num          int,
+   collect_num          int,
+   is_top               bool,
    primary key (id)
 );
 
 alter table bms_blog comment '博客表';
+
+/*==============================================================*/
+/* Table: bms_blog_reply                                        */
+/*==============================================================*/
+create table bms_blog_reply
+(
+   id                   bigint not null,
+   blog_id              bigint,
+   to_reply_id          bigint,
+   replyer_id           bigint,
+   content              varchar(1000),
+   create_time          datetime,
+   agree_num            int,
+   status               tinyint,
+   is_top               bool,
+   primary key (id)
+);
+
+alter table bms_blog_reply comment '博客回复表';
 
