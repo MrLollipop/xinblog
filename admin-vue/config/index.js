01ee619202d50887f94b9b9abd478785d513ab36
haoxin_2014 <haoxin_2014@163.com>
2021-09-09 21:29:54 +0800

    启动检查修改

--- admin-vue/config/index.js
+++ admin-vue/config/index.js
@@ -33,7 +33,8 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    // 启动VUE时，检查Eslint报错，故关闭 
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
