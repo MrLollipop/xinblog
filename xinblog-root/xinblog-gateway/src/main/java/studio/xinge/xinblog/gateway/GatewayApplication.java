7f7ccddcc8ad43ca47eeb8aeae102e4ff89c9313
haoxin_2014 <haoxin_2014@163.com>
2021-09-15 21:51:01 +0800

    gateway网关微服务，调整cloud版本

---
+++ xinblog-root/xinblog-gateway/src/main/java/studio/xinge/xinblog/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package studio.xinge.xinblog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author xinge
+ * @E-mail haoxin_2014@163.com
+ * @Date 2021/09/12
+ * @Description
+ */
+@SpringBootApplication()
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
