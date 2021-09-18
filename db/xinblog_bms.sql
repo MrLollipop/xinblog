c7d4fc8a4f6ddb1c6677a34b4be20013466cd385
haoxin_2014 <haoxin_2014@163.com>
2021-09-18 17:36:29 +0800

    博客字段默认值

--- db/xinblog_bms.sql
+++ db/xinblog_bms.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2021/9/1 11:01:40                            */
+/* Created on:     2021/9/18 14:30:34                           */
 /*==============================================================*/
 
 
@@ -13,16 +13,16 @@ drop table if exists bms_blog_reply;
 /*==============================================================*/
 create table bms_blog
 (
-   id                   bigint not null auto_increment,
+   id                   bigint not null,
    title                varchar(30),
    content              varchar(1000),
    status               tinyint comment '0删除，1正常，2草稿',
    create_time          datetime,
    update_time          datetime,
-   like_num             int,
-   forward_num          int,
-   collect_num          int,
-   is_top               bool,
+   like_num             int default 0,
+   forward_num          int default 0,
+   collect_num          int default 0,
+   is_top               bool default false,
    primary key (id)
 );
 
@@ -39,9 +39,9 @@ create table bms_blog_reply
    replyer_id           bigint,
    content              varchar(1000),
    create_time          datetime,
-   agree_num            int,
-   status               tinyint,
-   is_top               bool,
+   agree_num            int default 0,
+   status               tinyint default 1 comment '0删除，1正常',
+   is_top               bool default false,
    primary key (id)
 );
 
