0a7023ea4a2cf29abe7268fdd093609f26bddd65
haoxin_2014 <haoxin_2014@163.com>
2021-09-20 10:10:22 +0800

    更新测试方法

--- xinblog-root/xinblog-blog/src/test/java/studio/xinge/xinblog/blog/XinblogBlogApplicationTests.java
+++ xinblog-root/xinblog-blog/src/test/java/studio/xinge/xinblog/blog/XinblogBlogApplicationTests.java
@@ -4,14 +4,15 @@ import cn.hutool.core.util.RandomUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import studio.xinge.xinblog.blog.entity.BlogEntity;
 import studio.xinge.xinblog.blog.service.BlogService;
+import studio.xinge.xinblog.common.utils.Constant;
+import studio.xinge.xinblog.common.utils.PageUtils;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 
 @SpringBootTest()
 @ActiveProfiles("uat")
@@ -27,12 +28,16 @@ class XinblogBlogApplicationTests {
     @Test
     void save(){
         BlogEntity blogEntity = new BlogEntity();
-        blogEntity.setTitle("第3篇博客");
+        blogEntity.setTitle("第n篇博客");
         blogEntity.setContent("内容内容XXXXdsddfasdf");
-        blogEntity.setStatus(1);
+        blogEntity.setStatus(new int[]{0,1,2}[RandomUtil.randomInt(0,3)]);
         Date date = new Date();
         blogEntity.setCreateTime(date);
         blogEntity.setUpdateTime(date);
+        blogEntity.setLikeNum(RandomUtil.randomInt(1,200));
+        blogEntity.setForwardNum(RandomUtil.randomInt(1,200));
+        blogEntity.setCollectNum(RandomUtil.randomInt(1,200));
+        blogEntity.setIsTop(false);
 
         blogService.save(blogEntity);
     }
@@ -60,5 +65,18 @@ class XinblogBlogApplicationTests {
         }
 
         blogService.saveBatch(list, 1000);
+    }
+
+    @Test
+    void query() {
+
+        HashMap<String, Object> conditions = new HashMap<>();
+        conditions.put(Constant.PAGE, "20");
+        conditions.put(Constant.LIMIT, "2");
+
+        conditions.put("status", "0");
+
+        PageUtils pageUtils = blogService.queryPage(conditions);
+        System.out.println(pageUtils.getList());
     }
 }
