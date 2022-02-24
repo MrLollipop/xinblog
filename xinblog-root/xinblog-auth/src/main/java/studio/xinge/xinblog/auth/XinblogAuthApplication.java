cd7900ba3711c3ab0bf5af1f4f93a6023e447a69
haoxin_2014 <haoxin_2014@163.com>
2022-02-24 09:21:35 +0800

    用户认证与权限管理

---
+++ xinblog-root/xinblog-auth/src/main/java/studio/xinge/xinblog/auth/XinblogAuthApplication.java
@@ -0,0 +1,13 @@
+package studio.xinge.xinblog.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinblogAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinblogAuthApplication.class, args);
+    }
+
+}
---
+++ xinblog-root/xinblog-auth/src/test/java/studio/xinge/xinblog/auth/XinblogAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.xinge.xinblog.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinblogAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
