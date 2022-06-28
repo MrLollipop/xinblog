8749b9938e3e107ef33296dc3aff19dcac4a9ff9
haoxin_2014 <haoxin_2014@163.com>
2022-06-28 22:04:28 +0800

    markdown 预览

--- xinblog-vue/src/components/Detail.vue
+++ xinblog-vue/src/components/Detail.vue
@@ -1,20 +1,44 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <markdown :content="content"></markdown>
   </div>
 </template>
 
 <script>
+import Markdown from "./Markdown.vue";
 export default {
-    data(){
-        return{
-            msg : 'aa',
-        };
-    },
-    mounted() {
+  components: { Markdown },
+  data() {
+    return {
+      msg: "aa",
+      content: "",
+    };
+  },
+  mounted() {
     this.$emit("bannerTitle", ["文章标题", "文章幅标题，可以详细说明"]);
-    this.msg=this.$route.query.blogId;
+    this.msg = this.$route.query.blogId;
+    this.getBlog();
+  },
+  activated() {
+    this.getBlog();
+  },
+  methods: {
+    getBlog() {
+      this.$http({
+        url: this.$http.adornUrl(
+          "5.Spring5-%E6%B3%A8%E8%A7%A3%E9%A9%B1%E5%8A%A8%E5%BC%80%E5%8F%91-1.%E7%BB%84%E4%BB%B6%E6%B3%A8%E5%86%8C.md"
+        ),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("ss");
+        this.content = data;
+      });
     },
+  },
+  created() {
+    // this.content = "# 测试";
+  },
 };
 </script>
 
