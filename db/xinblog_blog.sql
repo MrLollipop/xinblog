64e5f934ff496faf1ca1f520ff70a28080950814
haoxin_2014 <haoxin_2014@163.com>
2022-07-07 21:27:34 +0800

    更新建表语句

--- db/xinblog_blog.sql
+++ db/xinblog_blog.sql
@@ -17,8 +17,7 @@ CREATE TABLE `t_blog` (
   `id` bigint(20) NOT NULL,
   `title` varchar(30) DEFAULT NULL,
   `sub_title` varchar(100) DEFAULT NULL,
-  `cover` varchar(500) DEFAULT NULL,
-  `content` varchar(1000) DEFAULT NULL,
+  `cover` varchar(500) DEFAULT NULL COMMENT '博客封面地址',
   `status` tinyint(4) DEFAULT NULL COMMENT '0删除，1正常，2草稿',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
@@ -27,7 +26,9 @@ CREATE TABLE `t_blog` (
   `collect_num` int(11) DEFAULT '0',
   `top` tinyint(1) DEFAULT '0',
   `view_num` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `markdown_addr` varchar(500) DEFAULT NULL COMMENT 'markdown地址',
+  PRIMARY KEY (`id`),
+  KEY `t_blog_status_IDX` (`status`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='博客表';
 
 /*==============================================================*/
