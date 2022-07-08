a691351c32c06057d419ce279aa845fda38a196a
haoxin_2014 <haoxin_2014@163.com>
2022-07-08 17:42:14 +0800

    Redisson配置类简化

--- xinblog-root/xinblog-blog/src/main/java/studio/xinge/xinblog/blog/config/RedissonConfig.java
+++ xinblog-root/xinblog-blog/src/main/java/studio/xinge/xinblog/blog/config/RedissonConfig.java
@@ -1,5 +1,6 @@
 package studio.xinge.xinblog.blog.config;
 
+import lombok.Data;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
@@ -7,8 +8,10 @@ import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.redisson.config.TransportMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author xinge
@@ -20,7 +23,7 @@ import org.springframework.context.annotation.Configuration;
 public class RedissonConfig {
 
     @Autowired
-    RedissonSingleConfig singleConfig;
+    SingleConfig singleConfig;
 
     @Bean
     public RedissonClient redissonClient() {
@@ -47,5 +50,28 @@ public class RedissonConfig {
     }
 
 }
+@Component
+@ConfigurationProperties("single")
+@Data
+class SingleConfig {
+    //可以用"rediss://"来启用SSL连接
+    private String address;
+    private String password;
+    private int database;
+    //    最小保持连接数
+    private int connectionMinimumIdleSize;
+    //    连接池大小
+    private int connectionPoolSize;
+    //    限制连接释放时间
+    private int idleConnectionTimeout;
+    //    连接超时 毫秒
+    private int connectTimeout;
+    //    命令超时 毫秒
+    private int timeout;
+    //    命令重试次数
+    private int retryAttempts;
+    //    命令重试间隔 毫秒
+    private int retryInterval;
 
+}
 
