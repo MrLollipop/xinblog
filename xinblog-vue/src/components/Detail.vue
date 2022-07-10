48aa1e541122064cb3034bfe983379c4b6cc052e
haoxin_2014 <haoxin_2014@163.com>
2022-07-10 11:54:21 +0800

    详情页布局调整 数据填充

--- xinblog-vue/src/components/Detail.vue
+++ xinblog-vue/src/components/Detail.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <markdown :content="content"></markdown>
-    <el-page-header @back="goBack" content="详情页面"> </el-page-header>
+    <el-row type="flex" justify="center" :gutter="40">
+      <div class="markdown">
+        <el-page-header class="back" @back="goBack" content="主页" />
+        <!-- <h1 style="font-size:30px">{{ title }}</h1> -->
+        <el-row type="flex" class="row" justify="space-around">
+            <el-col :span="12"><span style="float:left">浏览量：{{ viewNum }}</span></el-col>
+            <el-col :span="12"><span style="float:right">最后更新时间：{{ updateTime }}</span></el-col>
+        </el-row>    
+        <img class="headPic" :src="cover"/>
+        <markdown :content="content"></markdown>
+        <el-page-header class="back" @back="goBack" content="主页" />
+      </div>
+    </el-row>
   </div>
 </template>
 
 <script>
 import Markdown from "./Markdown.vue";
+import { formatDate } from "@/utils";
+
 export default {
   components: { Markdown },
   data() {
@@ -15,6 +27,9 @@ export default {
       title: "文章加载中...",
       subTitle: "",
       markdownAddr: "",
+      updateTime: "",
+      viewNum: "",
+      cover: "",
       content: "",
     };
   },
@@ -33,6 +48,9 @@ export default {
           this.title = data.blog.title;
           this.subTitle = data.blog.subTitle;
           this.markdownAddr = data.blog.markdownAddr;
+          this.updateTime = formatDate(data.blog.updateTime);
+          this.cover = data.blog.cover;
+          this.viewNum = data.blog.viewNum;
 
           this.$emit("bannerTitle", [this.title, this.subTitle]);
 
@@ -57,10 +75,27 @@ export default {
     },
     goBack() {
       console.log("go back");
+      history.go(-1);
     },
   },
 };
 </script>
 
 <style scoped>
+.markdown {
+  width: 50vw;
+  margin: 2px 0px;
+}
+.back {
+  margin-top: 15px;
+  margin-bottom: 10px;
+}
+.row {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.headPic {
+  width: 60%;
+  margin-bottom: 20px;
+}
 </style>
