05b774e0d7b1c7dd93a97d81e2847eeb9867549e
haoxin_2014 <haoxin_2014@163.com>
2022-07-17 19:08:27 +0800

    专题页面基本内容填充

--- xinblog-vue/src/components/Topic.vue
+++ xinblog-vue/src/components/Topic.vue
@@ -4,13 +4,13 @@
     <el-row type="flex" justify="center" :gutter="40">
       <div class="topic">
         <tag :tagVOList="tagVOList"></tag>
-        key: {{ this.$route.params.key }}
         <el-row :gutter="60">
           <el-col :span="8" v-for="item in blogs" :key="item.id">
             <router-link :to="{ path: 'detail', query: { blogId: item.id } }">
               <blog-card :blogEntity="item"></blog-card>
             </router-link>
           </el-col>
+          <el-empty :description="noDataMsg" v-show="noDataShow"></el-empty>
         </el-row>
       </div>
     </el-row>
@@ -18,17 +18,29 @@
 </template>
  
 <script>
-import BlogCard from './index/BlogCard.vue';
+import BlogCard from "./index/BlogCard.vue";
 import Tag from "./index/Tag.vue";
 export default {
   components: { Tag, BlogCard },
   name: "Hot",
   data() {
     return {
-      tagVOList: [],
       tagKey: this.$route.params.key,
+      tagVOList: [],
       blogs: [],
+      noDataMsg: "",
+      noDataShow: false,
     };
+  },
+  watch: {
+    // 监听路由变化 $route.path == this.$route.path
+    "$route.path": function (newVal, oldVal) {
+      console.log(newVal);
+      this.blogs = [];
+      this.noDataShow = false;
+      this.tagKey = this.$route.params.key;
+      this.getBlogsList();
+    },
   },
   mounted() {
     this.$emit("bannerTitle", ["欣 哥 工 作 室", "专题清单"]);
@@ -49,18 +61,22 @@ export default {
         }
       });
     },
-    // 获取缓存Tags清单
+    // 获取标签所属博客清单
     getBlogsList() {
       this.$http({
         url: this.$http.adornUrl("api/blog/user/blogs/tag"),
         method: "get",
         params: this.$http.adornParams({
-          key: this.$route.params.key,
+          key: this.tagKey,
         }),
       }).then(({ data }) => {
         console.log(data);
         if (data.code === 10000) {
           this.blogs = data.blogs;
+        } else if (data.code === 10006) {
+          this.noDataMsg = data.msg;
+          this.noDataShow = true;
+        } else {
         }
       });
     },
