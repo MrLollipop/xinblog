73c03052ae59b7ee2e70ea7b31d4b16969ee73e7
haoxin_2014 <haoxin_2014@163.com>
2022-07-17 19:36:09 +0800

    相同路由重复点击问题修复

--- xinblog-vue/src/router/index.js
+++ xinblog-vue/src/router/index.js
@@ -7,6 +7,12 @@ import Topic from '@/components/Topic'
 
 Vue.use(Router)
 
+const originalPush = Router.prototype.push
+
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 export default new Router({
   routes: [
     {
