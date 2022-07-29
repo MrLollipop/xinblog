e67d98f34aca0294f29f1b7f234f840d281873da
haoxin_2014 <haoxin_2014@163.com>
2022-07-29 17:15:52 +0800

    VO构造、比较方法

--- xinblog-root/xinblog-blog/src/main/java/studio/xinge/xinblog/blog/vo/TBlogReplyVO.java
+++ xinblog-root/xinblog-blog/src/main/java/studio/xinge/xinblog/blog/vo/TBlogReplyVO.java
@@ -71,4 +71,23 @@ public class TBlogReplyVO implements Serializable {
 
     private Integer agreeNum;
 
+    public TBlogReplyVO() {
+    }
+
+    public TBlogReplyVO(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TBlogReplyVO that = (TBlogReplyVO) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
