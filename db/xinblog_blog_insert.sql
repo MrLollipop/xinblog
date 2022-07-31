c16460504d1ef3edd5a2726cbf0c5c12eed1529d
haoxin_2014 <haoxin_2014@163.com>
2022-07-31 21:52:12 +0800

    插入脚本

---
+++ db/xinblog_blog_insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO xinblog_blog.t_tag (label,disabled) VALUES
+	 ('面试题精选',0),
+	 ('Spring',0),
+	 ('MySQL',0),
+	 ('Spring Cloud',0),
+	 ('Spring Boot',0),
+	 ('Spring Cloud Alibaba',0),
+	 ('Redis',0),
+	 ('算法题',0),
+	 ('Leetcode',0),
+	 ('数据结构',0);
+INSERT INTO xinblog_blog.t_tag (label,disabled) VALUES
+	 ('分布式',0),
+	 ('高并发',0),
+	 ('Nginx',0);
+
+COMMIT;
