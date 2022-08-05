68b8b01cc3c93a97225db41d4f4eebd75247f2d5
haoxin_2014 <haoxin_2014@163.com>
2022-08-05 21:06:45 +0800

    生产api地址

--- admin-vue/static/config/index-prod.js
+++ admin-vue/static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://demo.open.renren.io/renren-fast-server';
+  window.SITE_CONFIG['baseUrl'] = 'http://47.98.130.159/gateway';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
