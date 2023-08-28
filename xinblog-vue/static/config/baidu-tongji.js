64d3645aaf98a91546b749b508e5add43acd6f5e
haoxinchen <haoxin_2014@163.com>
2023-08-28 16:01:27 +0800

    添加百度统计

---
+++ xinblog-vue/static/config/baidu-tongji.js
@@ -0,0 +1,8 @@
+// 百度访问量统计
+var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?1ddcd7543aba82201c96b5530ca350ab";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
